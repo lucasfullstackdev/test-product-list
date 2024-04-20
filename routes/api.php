1ab84b3233370cfac598a69886d70b7ab8b68cb2
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-04-19 22:29:11 -0300

    refactor: adding route prefix to application bootstrap

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1.0')->group(function () {
-  Route::prefix('products')->group(function () {
-    require __DIR__ . '/api/v1.0/products.php';
-  });
+Route::prefix('products')->group(function () {
+  require __DIR__ . '/api/v1.0/products.php';
 });
