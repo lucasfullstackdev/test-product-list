a8ec54939d6b38678a9324347cde37345c5d2220
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-04-19 22:26:26 -0300

    feat: creating routes for listing products

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1.0')->group(function () {
+  Route::prefix('products')->group(function () {
+    require __DIR__ . '/api/v1.0/products.php';
+  });
+});
