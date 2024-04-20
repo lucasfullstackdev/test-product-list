d8b578edcee6468d050a42b17b1c9a712e7328fa
lucas oliveira <lucas.fullstack.dev@gmail.com>
2024-04-19 22:30:02 -0300

    refactor: simplifying loading of routes for products

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('products')->group(function () {
-  require __DIR__ . '/api/v1.0/products.php';
-});
+Route::prefix('products')->group(fn () => require __DIR__ . '/api/v1.0/products.php');
